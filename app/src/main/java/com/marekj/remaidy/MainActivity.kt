f546b9f57433cef184da5966fdaf977c7dc70c9e
Marek <56512676+jzmark@users.noreply.github.com>
2023-11-23 23:08:07 +0000

    Logo in login page, drawer in main menu

--- app/src/main/java/com/marekj/remaidy/MainActivity.kt
+++ app/src/main/java/com/marekj/remaidy/MainActivity.kt
@@ -2,10 +2,21 @@ package com.marekj.remaidy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_menu)
+        setContentView(R.layout.main_menu)
+
+        //drawer actions
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        topAppBar.setNavigationOnClickListener {
+            val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+            drawerLayout.open()
+        }
     }
+
 }
