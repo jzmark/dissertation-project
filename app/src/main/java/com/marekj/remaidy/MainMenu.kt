d8b8527b26a8632e3d880c879cd17650f9ebcb29
Marek <56512676+jzmark@users.noreply.github.com>
2023-11-24 15:42:51 +0000

    Forced horizontal orientation, static drawer

--- app/src/main/java/com/marekj/remaidy/MainMenu.kt
+++ app/src/main/java/com/marekj/remaidy/MainMenu.kt
@@ -3,27 +3,25 @@ package com.marekj.remaidy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
+        drawerListener()
 
-        //drawer actions
-        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        topAppBar.setNavigationOnClickListener {
-            drawerLayout.open()
-        }
+    }
 
+    private fun drawerListener() {
         val navigationView = findViewById<NavigationView>(R.id.navigation)
         navigationView.setNavigationItemSelectedListener { menuItem ->
             // Handle menu item selected
             menuItem.isChecked = true
-            drawerLayout.close()
             if(menuItem.itemId == R.id.logoutDrawer) {
                 val menuIntent = Intent(this, MainActivity::class.java)
                 startActivity(menuIntent)
