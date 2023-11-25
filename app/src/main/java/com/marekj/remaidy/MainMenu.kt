d6df4d1d3099192bc10a974c444c335fa1a34069
Marek <56512676+jzmark@users.noreply.github.com>
2023-11-25 18:20:31 +0000

    New main menu card design

--- app/src/main/java/com/marekj/remaidy/MainMenu.kt
+++ app/src/main/java/com/marekj/remaidy/MainMenu.kt
@@ -3,19 +3,26 @@ package com.marekj.remaidy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.Settings
+import android.widget.Button
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
         drawerListener()
+        buttonListener()
     }
 
+    private fun buttonListener() {
+        val button = findViewById<Button>(R.id.mainMenuButton)
+        button.setOnClickListener() {
+            startActivity(Intent(Settings.ACTION_DISPLAY_SETTINGS))
+        }
+    }
     private fun drawerListener() {
         val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
