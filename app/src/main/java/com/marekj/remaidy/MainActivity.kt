bf44c6a81b452ba8e088416a518bcb9ab26820ed
Marek <56512676+jzmark@users.noreply.github.com>
2024-02-20 23:02:13 +0000

    Removing login menu in line with suggestions

--- app/src/main/java/com/marekj/remaidy/MainActivity.kt
+++ app/src/main/java/com/marekj/remaidy/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    //TODO: FIX DARK MODE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_menu)
