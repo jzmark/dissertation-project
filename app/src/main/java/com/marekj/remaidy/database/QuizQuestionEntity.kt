31ac6006b1374b841d7f5b8c7cde9a3dc7b82c3f
Marek <56512676+jzmark@users.noreply.github.com>
2024-04-21 01:34:06 +0100

    Question is now displayed in quiz mode

--- app/src/main/java/com/marekj/remaidy/database/QuizQuestionEntity.kt
+++ app/src/main/java/com/marekj/remaidy/database/QuizQuestionEntity.kt
@@ -3,7 +3,7 @@ package com.marekj.remaidy.database
 class QuizQuestionEntity(
     val id: String = "null",
     val description: String = "null",
-    val answer1Correct: String = "null", val answer2: String = "null",
+    val answer1: String = "null", val answer2: String = "null",
     val answer3: String = "null", val answer4: String = "null",
     val imgPath: String = "null", val correctAnswerID: String = "null",
     val isAnsweredCorrectly: String = "false"
--- app/src/main/java/com/marekj/remaidy/patientview/QuizMode.kt
+++ app/src/main/java/com/marekj/remaidy/patientview/QuizMode.kt
@@ -1,18 +1,69 @@
 package com.marekj.remaidy.patientview
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 import com.marekj.remaidy.R
+import com.marekj.remaidy.controlpanel.ControlPanel
 import com.marekj.remaidy.database.QuizDatabase
+import com.marekj.remaidy.database.QuizQuestionEntity
+import java.io.File
 
 class QuizMode : AppCompatActivity() {
+    private lateinit var question : QuizQuestionEntity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.display_question)
+        question = QuizDatabase(this).getQuestionAndUpdate()
+        initialiseLayout()
+        drawerListener()
     }
 
-    fun initialiseLayout() {
-        val question = QuizDatabase(this).getQuestionAndUpdate()
+    private fun initialiseLayout() {
+        val imageFile = File(filesDir, question.imgPath)
+        val imageUri = Uri.fromFile(imageFile)
+        findViewById<ImageView>(R.id.imageDisplay).setImageURI(imageUri)
+        findViewById<TextView>(R.id.questionDescription).text = question.description
+        findViewById<Button>(R.id.ans1).text = question.answer1
+        findViewById<Button>(R.id.ans2).text = question.answer2
+        findViewById<Button>(R.id.ans3).text = question.answer3
+        findViewById<Button>(R.id.ans4).text = question.answer4
+    }
 
+    private fun drawerListener() {
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
+        }
+        val navigationView = findViewById<NavigationView>(R.id.navigation)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            drawerLayout.close()
+            if (menuItem.itemId == R.id.mainMenu) {
+                val menuIntent = Intent(this, MainMenu::class.java)
+                startActivity(menuIntent)
+                finish()
+                false
+            }
+            if (menuItem.itemId == R.id.controlPanel) {
+                val menuIntent = Intent(this, ControlPanel::class.java)
+                startActivity(menuIntent)
+                finish()
+                false
+            }
+            else {
+                menuItem.isChecked = false
+                false
+            }
+        }
     }
 }
